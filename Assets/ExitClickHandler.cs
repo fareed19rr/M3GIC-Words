b37dc6d4b3f125cc4688d228ce51e6ccbf0750bc
Muhamad Farid Ridho Rambe <muhamadfaridrr@gmail.com>
2021-05-08 14:17:38 +0700

    First Commit

---
+++ Assets/ExitClickHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitClickHandler : MonoBehaviour
+{
+    public void doquit()
+    {
+        Application.Quit();
+    }
+}
